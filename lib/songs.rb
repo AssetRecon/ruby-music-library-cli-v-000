dcff6ffb5e4d288d46bb6c0fd9393db2482f5cbd
Edsel Caprice <caprice@mail.usf.edu>
2018-07-17 03:47:04 +0000

    automatically backed up by learn

--- lib/songs.rb
+++ lib/songs.rb
@@ -47,11 +47,18 @@ class Song
   genre = Genre.find_or_create_by_name(genre_string)
 
   Song.new(song_string,artist,genre)
-
   end
 
   def self.create_from_filename
     # does the same thing as .new_from_filename but also saves the newly-created song to the @@all class variable.
+    song_string = filename.split("-")[1].strip
+    artist_string = filename.split("-")[0].strip
+    genre_string = filename.split("-")[2].split(".")[0].strip
+
+    artist = Artist.find_or_create_by_name(artist_string)
+    genre = Genre.find_or_create_by_name(genre_string)
+
+    Song.new(song_string,artist,genre)
   end
   def self.create(name)
     song = Song.new(name)
