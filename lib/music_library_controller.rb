6983ad1f3a5325d0c7d76ebd1da474e528e5fd55
Edsel Caprice <caprice@mail.usf.edu>
2018-07-17 04:00:16 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,10 +1,10 @@
 class MusicLibraryController
   attr_accessor :path
-  def initialize(path)
+  def initialize(path = './db/mp3s')
     @path = path
-    
+    MusicImporter.new(path)
   end
-  
+
 
 
 
